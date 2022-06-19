7eac99c2d4b0a9ccfedf5a374c425ce58ddb944f
MohammedTarekT1M <302923562@student.rocmondriaan.nl>
2022-06-19 17:58:43 +0200

    Initial commit

---
+++ src/data/countryData.js
@@ -0,0 +1,29 @@
+const countryData = {
+    countries: [
+        {
+            id: 1,
+            name:'USA',
+            capital: 'Washington',
+            cost: 1250,
+            details: 'USA are among the most visited',
+            img: 'washington.jpg'
+        },
+        {
+            id: 2,
+            name:'Netherlands',
+            capital: 'Amsterdam',
+            cost: 795,
+            details: 'Amsterdam are among the most visited',
+            img: 'amsterdam.jpg'
+        },
+        {
+            id: 3,
+            name:'Germany',
+            capital: 'Berlin',
+            cost: 900,
+            details: 'The capital of Germany',
+            img: 'berlin.jpg'
+        },
+    ]
+}
+export default countryData;
